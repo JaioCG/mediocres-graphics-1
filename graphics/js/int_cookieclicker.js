af67d74aa90fa267dbe8645702584ac73c6a147f
Jaio <jaio0701@gmail.com>
2023-06-13 14:29:59 -0400

    added cookie clicker :)

--- graphics/js/int_cookieclicker.js
+++ graphics/js/int_cookieclicker.js
@@ -1,21 +1,31 @@
-// Twitch channel ID for ScottMadeThis.
-// Replace with your own channel ID.
-const channel = 97032862;
+// MediocresEvents Channel ID
+const channel = 603499545;
 
-// Connect to the Heat back-end.
+// Connect to the Heat back-end
 let url = `wss://heat-api.j38.net/channel/${channel}`;
 let ws = new WebSocket(url);
 
-// Connection successful. 
+// Connection successful.
 ws.addEventListener('open', () => {
     console.log(`Connection open to Heat API server, channel ${channel}.`);
 });
 
+// Create cookie counter
+const cookieCountText = document.getElementById('cookie-count');
+let cookieCount = localStorage.getItem("cookie-count") || 0;
+
 // Message received.
 ws.addEventListener('message', (message) => {
-    // Parse message data.
+    // Parse message data and log to console
     let data = JSON.parse(message.data);
-
-    // Write to console.
     console.log(data);
+
+    // Update cookie count if registered click is in the Cookie Zone(tm)
+    if((data.x >= (50/1920) && data.x <= (350/1920)) && (data.y >= (730/1080) && data.y <= (1030/1080))) {
+        document.getElementById("cookie-click-animation").style.animation = "cookie-click-in 0.1s";
+        setTimeout(function() { document.getElementById("cookie-click-animation").style.animation = "cookie-click-out 0.1s"; }, 100);
+        cookieCount++;
+        cookieCountText.innerHTML = cookieCount;
+        localStorage.setItem("cookie-count", cookieCount);
+    }
 });
